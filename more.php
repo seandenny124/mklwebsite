8f5ff3ce698a868a6985c694c250f948b46f4a13
Sean Denny <seandenny124@u.boisestate.edu>
2024-04-07 12:38:23 -0600

    f

--- more.php
+++ more.php
@@ -173,7 +173,8 @@ if(isset($_SESSION["email"])){
         $stmt_check = $conn->prepare("SELECT comments.*, comments.id AS cid, users.* FROM comments JOIN users ON comments.username = users.email");
     }
     else {
-    $stmt_check = $conn->prepare("SELECT comments.*, comments.id AS cid, users.* FROM comments JOIN users ON comments.username = users.email WHERE comments.username = ?");
+        $stmt_check = $conn->prepare("SELECT comments.*, comments.id AS cid, users.* FROM comments JOIN users ON comments.username = users.email");
+    //$stmt_check = $conn->prepare("SELECT comments.*, comments.id AS cid, users.* FROM comments JOIN users ON comments.username = users.email WHERE comments.username = ?");
         $stmt_check->bind_param("s", $_SESSION["email"]);
     }
     

263784e9440576bbb77a0c83d3c961e7b5aa9cf2
Sean Denny <seandenny124@u.boisestate.edu>
2024-04-07 12:08:42 -0600

    juyh

--- more.php
+++ more.php
@@ -171,8 +171,12 @@ if(isset($_SESSION["email"])){
     <?php 
     if(isset($_SESSION) && $_SESSION['user_role'] == 1){
         $stmt_check = $conn->prepare("SELECT comments.*, comments.id AS cid, users.* FROM comments JOIN users ON comments.username = users.email");
-    }else{
+    }else if (isset($_SESSION) && $_SESSION['user_role'] == 2) {
         $stmt_check = $conn->prepare("SELECT comments.*, comments.id AS cid, users.* FROM comments JOIN users ON comments.username = users.email WHERE comments.username = ?");
+        $stmt_check->bind_param("s", $_SESSION["email"]);
+    }
+    else {
+    $stmt_check = $conn->prepare("SELECT comments.*, comments.id AS cid, users.* FROM comments JOIN users ON comments.username = users.email WHERE comments.username = ?");
         $stmt_check->bind_param("s", $_SESSION["email"]);
     }
     
